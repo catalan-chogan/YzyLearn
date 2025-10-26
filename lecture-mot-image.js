495a3f6e0d8eb614193dd07fe308e2e95350bdbe
Vl4dou <catalancedric@yahoo.fr>
2025-10-26 09:54:59 +0100

    ajout niveau 2 lecture mots

--- lecture-mot-image.js
+++ lecture-mot-image.js
@@ -1,21 +1,81 @@
-const mots = ["OS", "ANE", "SAC", "COL", "BAL", "VIS", "OR", "BOL", "AS", "BUS", "COR", "MUR"];
-const images = {
-  "OS": "Img/lecture-mots/os.png",
-  "ANE": "Img/lecture-mots/ane.png",
-  "SAC": "Img/lecture-mots/sac.png",
-  "COL": "Img/lecture-mots/col.png",
-  "BAL": "Img/lecture-mots/bal.png",
-  "VIS": "Img/lecture-mots/vis.png",
-  "OR": "Img/lecture-mots/or.png",
-  "BOL": "Img/lecture-mots/bol.png",
-  "AS": "Img/lecture-mots/as.png",
-  "BUS": "Img/lecture-mots/bus.png",
-  "COR": "Img/lecture-mots/cor.png",
-  "MUR": "Img/lecture-mots/mur.png"
+// Récupère le niveau depuis l’URL (ex: lecture-mot-image.html?level=2)
+const params = new URLSearchParams(window.location.search);
+const niveau = parseInt(params.get("level")) || 1;
+
+// 🔤 Définition des niveaux
+const niveaux = {
+  1: {
+    mots: ["OS", "ANE", "SAC", "COL", "BAL", "VIS", "OR", "BOL", "AS", "BUS", "COR", "MUR"],
+    images: {
+      "OS": "Img/lecture-mots/niv2/os.png",
+      "ANE": "Img/lecture-mots/niv2/ane.png",
+      "SAC": "Img/lecture-mots/niv2/sac.png",
+      "COL": "Img/lecture-mots/niv2/col.png",
+      "BAL": "Img/lecture-mots/niv2/bal.png",
+      "VIS": "Img/lecture-mots/niv2/vis.png",
+      "OR": "Img/lecture-mots/niv2/or.png",
+      "BOL": "Img/lecture-mots/niv2/bol.png",
+      "AS": "Img/lecture-mots/niv2/as.png",
+      "BUS": "Img/lecture-mots/niv2/bus.png",
+      "COR": "Img/lecture-mots/niv2/cor.png",
+      "MUR": "Img/lecture-mots/niv2/mur.png"
+    }
+  },
+  2: {
+    mots: ["TOMATE", "BANANE", "CARTABLE", "CABANE", "PIRATE", "SALADE", "LIMACE", "LIVRE", "PATATE", "PARAPLUIE", "CATAPULTE", "ROBOT", "USINE", "NUAGE", "VALISE", "LAVABO", "PORTE", "PLUME", "TORTUE", "CARAVANE", "CAROTTE", "LAMA"],
+    images: {
+      "TOMATE": "Img/lecture-mots/niv2/tomate.png",
+      "BANANE": "Img/lecture-mots/niv2/banane.png",
+      "CARTABLE": "Img/lecture-mots/niv2/cartable.png",
+      "CABANE": "Img/lecture-mots/niv2/cabane.png",
+      "PIRATE": "Img/lecture-mots/niv2/pirate.png",
+      "SALADE": "Img/lecture-mots/niv2/salade.png",
+      "LIMACE": "Img/lecture-mots/niv2/limace.png",
+      "LIVRE": "Img/lecture-mots/niv2/livre.png",
+      "PATATE": "Img/lecture-mots/niv2/patate.png",
+      "PARAPLUIE": "Img/lecture-mots/niv2/parapluie.png",
+      "CATAPULTE": "Img/lecture-mots/niv2/catapulte.png",
+      "ROBOT": "Img/lecture-mots/niv2/robot.png",
+      "USINE": "Img/lecture-mots/niv2/usine.png",
+      "NUAGE": "Img/lecture-mots/niv2/nuage.png",
+      "VALISE": "Img/lecture-mots/niv2/valise.png",
+      "LAVABO": "Img/lecture-mots/niv2/lavabo.png",
+      "PORTE": "Img/lecture-mots/niv2/porte.png",
+      "PLUME": "Img/lecture-mots/niv2/plume.png",
+      "TORTUE": "Img/lecture-mots/niv2/tortue.png",
+      "CARAVANE": "Img/lecture-mots/niv2/caravane.png",
+      "CAROTTE": "Img/lecture-mots/niv2/carotte.png",
+      "LAMA": "Img/lecture-mots/niv2/lama.png"
+    }
+  }
 };
 
+// 🔧 Sélection du bon niveau
+const mots = niveaux[niveau].mots;
+const images = niveaux[niveau].images;
 let motChoisi = "";
 
+// 🎲 Listes d'images aléatoires pour le feedback
+const imagesGagne = [
+  "Img/lecture-mots/gagne/gagne1.jpg",
+  "Img/lecture-mots/gagne/gagne2.jpg",
+  "Img/lecture-mots/gagne/gagne3.jpg",
+  "Img/lecture-mots/gagne/gagne4.jpg",
+  "Img/lecture-mots/gagne/gagne5.jpg",
+  "Img/lecture-mots/gagne/gagne6.jpg",
+  "Img/lecture-mots/gagne/gagne7.jpg",
+  "Img/lecture-mots/gagne/gagne8.jpg",
+  "Img/lecture-mots/gagne/gagne9.jpg"
+];
+
+const imagesPerdu = [
+  "Img/lecture-mots/perdu/perdu1.jpg",
+  "Img/lecture-mots/perdu/perdu2.jpg",
+  "Img/lecture-mots/perdu/perdu3.jpg",
+  "Img/lecture-mots/perdu/perdu4.jpg"
+];
+
+// Mélange un tableau
 function melanger(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -24,9 +84,10 @@ function melanger(array) {
   return array;
 }
 
+// Nouvelle question
 function nouvelleQuestion() {
   motChoisi = mots[Math.floor(Math.random() * mots.length)];
-  document.getElementById("mot-a-trouver").textContent = `${motChoisi}`;
+  document.getElementById("mot-a-trouver").textContent = motChoisi;
 
   const mauvaises = mots.filter(m => m !== motChoisi);
   const options = melanger([motChoisi, ...melanger(mauvaises).slice(0, 3)]);
@@ -44,15 +105,23 @@ function nouvelleQuestion() {
   });
 }
 
+// Vérifie la réponse
 function verifier(choix) {
   const estCorrect = choix === motChoisi;
   afficherFeedbackImage(estCorrect);
 }
 
+// Affiche image de succès/échec aléatoire
 function afficherFeedbackImage(reussi) {
   const overlay = document.getElementById("feedback-overlay");
   const img = document.getElementById("feedback-image");
-  img.src = reussi ? "Img/gagne.jpg" : "Img/perdu.jpg";
+
+  if (reussi) {
+    img.src = imagesGagne[Math.floor(Math.random() * imagesGagne.length)];
+  } else {
+    img.src = imagesPerdu[Math.floor(Math.random() * imagesPerdu.length)];
+  }
+
   overlay.classList.remove("hidden");
 
   setTimeout(() => {
@@ -62,4 +131,6 @@ function afficherFeedbackImage(reussi) {
   }, 2000);
 }
 
+// Démarrage
 document.addEventListener("DOMContentLoaded", nouvelleQuestion);
+document.getElementById("suivant").addEventListener("click", nouvelleQuestion);
